d08cbdd3fd6c227678b26390d77db871709c5bf3
Zach Cervi <zachcervi@gmail.com>
2022-06-07 16:49:55 -0600

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Zach Cervi on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
