cacda99ee414b15833e35bfd12e58a6b743b40f6
Zach Cervi <zachcervi@gmail.com>
2022-06-08 08:45:07 -0600

    added video model, video listitemview, and setup video UI

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct VideoListView: View {
+    //MARK: -PROPERTIES
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
+    
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+    
+    //MARK: -BODY
     var body: some View {
-        Text("Videos")
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                   VideoListItemView(video: item)
+                        .padding(.vertical, 8)
+                }//:LOOP
+            }//:LIST
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Button(action: {
+                        videos.shuffle()
+                        hapticImpact.impactOccurred()
+                    }){
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                }
+            }
+        }//:NAVIGATIONVIEW
     }
 }
 
+//MARK: -PREVIEW
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
         VideoListView()
