48408887815b6f814dadcddc0c7ab981e3631a32
Zach Cervi <zachcervi@gmail.com>
2022-06-10 15:15:46 -0600

    toggle icons, grid views

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -11,10 +11,30 @@ struct ContentView: View {
     //MARK: -PROPERTIES
     let animals: [Animal] = Bundle.main.decode("animals.json")
     let haptics = UIImpactFeedbackGenerator(style: .medium)
-    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
+  
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
     @State private var isGridViewActive: Bool = false
+    @State private var gridColumn: Int = 1
+    @State private var toolbarIcon: String = "square.grid.2x2"
     
-    //MARK: -BODY
+    //MARK: - FUNCTIONS
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: gridLayout.count % 3 + 1)
+        gridColumn = gridLayout.count
+        
+        //toolbar Image
+        switch gridColumn {
+        case 1:
+            toolbarIcon = "square.grid.2x2"
+        case 2:
+            toolbarIcon = "square.grid.3x2"
+        case 3:
+            toolbarIcon = "rectangle.grid.1x2"
+        default:
+            toolbarIcon = "square.grid.2x2"
+        }
+    }
+    //MARK: - BODY
     var body: some View {
         NavigationView {
             Group {
@@ -49,20 +69,20 @@ struct ContentView: View {
                     HStack(spacing: 16){
                         //LIST
                         Button(action: {
-                            print("This view is activated")
                             isGridViewActive = false
                             haptics.impactOccurred()
+                          
                         }){
                             Image(systemName: "square.fill.text.grid.1x2")
                                 .font(.title2)
                                 .foregroundColor(isGridViewActive ? .primary : .accentColor)
                         }
                         Button(action: {
-                            print("Grid view is activated")
                             isGridViewActive = true
                             haptics.impactOccurred()
+                            gridSwitch()
                         }){
-                            Image(systemName: "square.grid.2x2")
+                            Image(systemName: toolbarIcon)
                                 .font(.title2)
                                 .foregroundColor(isGridViewActive ? .accentColor : .primary)
                         }
